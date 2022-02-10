e8fffbf44b8a33e250b613607fce9d0a2ddb25f3
Purushothaman G <officialpurushothaman@gmail.com>
2022-02-10 18:15:19 +0530

    responsive part-1

--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -23,32 +23,36 @@ export default class Navbar extends React.Component {
         return (
             <section id="nav-section">
 
-                    <div id="uk-top">
+                <div id="uk-top">
 
-                        <div className="uk-card nav-card uk-card-body uk-width-1-2@m">
-                            <h3 className="uk-card-title brand-title">  <a id="brand" href="/"> <img alt="" src={logo} /> Sweetcups Bangalore </a> </h3>
-                            <div className="uk-width-1-2@m uk-text-center nav-link-section">
+                    <div className="uk-card nav-card uk-card-body uk-width-1-2@m">
+                        <h3 className="uk-card-title brand-title">  <a id="brand" href="/"> <img alt="" src={logo} /> Sweetcups Bangalore </a> </h3>
+                        <div className="uk-width-1-2@m uk-text-center nav-link-section">
 
-                                <ul className="nav-ul">
-                                    <li><a href="#feature-section"> About </a></li>
-                                    <li><a href="#contact-section"> Contact </a></li>
-                                    <li><a href="#items-section"> Services </a></li>
-                                    <li><a href="#gallery-section"> Gallery </a></li>
-                                </ul>
-                                
-                            </div>
+                            <ul className="nav-ul">
+                                <li><a href="#feature-section"> About </a></li>
+                                <li><a href="#contact-section"> Contact </a></li>
+                                <li><a href="#items-section"> Services </a></li>
+                                <li><a href="#gallery-section"> Gallery </a></li>
+                            </ul>
+
                         </div>
-
-                        <div className="uk-card vr-card uk-card-body">
-                            <h3 className="uk-card-title vr-title"> Wanna See Us ? </h3>
-                            <p className="vr-desc"> Click here to Experience Virtual Reality Based cake shop </p>
-                            <a href="http://ivandayas.tiiny.site" target='_blank'> <button className="uk-button vr-button"> Visit Us </button> </a>
-                        </div>
-
-
                     </div>
 
-                <div className="background-img">  </div>
+                    <div className="uk-card vr-card uk-card-body">
+                        <h3 className="uk-card-title vr-title"> Wanna See Us ? </h3>
+                        <p className="vr-desc"> Click here to Experience Virtual Reality Based cake shop </p>
+                        <a href="http://ivandayas.tiiny.site" target='_blank'> <button className="uk-button vr-button"> Visit Us </button> </a>
+                    </div>
+
+
+                </div>
+
+                <div className="uk-card vr-card-2 uk-card-body">
+                    <h3 className="uk-card-title vr-title"> Wanna See Us ? </h3>
+                    <p className="vr-desc"> Click here to Experience Virtual Reality Based cake shop </p>
+                    <a href="http://ivandayas.tiiny.site" target='_blank'> <button className="uk-button vr-button"> Visit Us </button> </a>
+                </div>
 
             </section>
         )
